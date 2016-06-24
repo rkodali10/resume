05ee9d15e9b7d3b318e5f5d12eb6598b33747212
paulm29 <paul.miscellaneous@gmail.com>
2016-06-25 08:37:59 +1000

    Complete jQuery version

--- resume_jq.js
+++ resume_jq.js
@@ -1,33 +1,48 @@
 $(document).ready(function() {
-    $("#about").hide();
     
-   $('div').mouseenter(function() {
+	/*
+   $('.item').mouseenter(function() {
        $(this).animate({
            height: '+=10px'
        });
    });
-   $('div').mouseleave(function() {
+   $('.item').mouseleave(function() {
        $(this).animate({
            height: '-=10px'
        }); 
    });
-   $('div').click(function() {
+   */
+   
+   $('.item').click(function(event) {
        //$('div').fadeIn(1000);
-       $('div').fadeTo('fast',1);
+	   //$(this).toggle(1000);
+       $('.item').fadeTo('fast',1);
        $(this).fadeTo('fast',0.25);
-       //$(this).toggle(1000);
+	   
+	   $('section').hide();
+	   link = event.target.id;
+	   console.log(link);
+	   section = "#" + link.substr(0,link.length-5);
+	   console.log(section);
+	   $('#contact').show();
+	   $(section).show();
    }); 
     
-    $('.link').click(function() {
-        $('.vanish').fadeOut('slow');
-    });
-    
-    $('.pull-me').click(function() {
-        $('.panel').slideToggle('slow');
-    });
+	$('section').hide();
+	$('#contact').show();
+	$('#career_objectives').show();
+	$('#career_objectives_link').fadeTo('fast',0.25);
 
+	writeEmail();
 });
 
-$(document).ready(function() {
-   $('#menu').accordion();
-});
+function writeEmail() {
+	var m = 'paul';  
+	m += '.';			
+	m += 'robotham';  
+	m += '@';
+	m += 'gmail';  
+	m += '.com';
+	document.getElementById('emailHref').href = "mailto:" + m;
+	document.getElementById('emailText').innerHTML = m;
+}
